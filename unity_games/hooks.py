00b7ad5c405f8d6b9cf40a015d00a89375ab6748
maskeddefender <amanupadhayay2906@gmail.com>
2026-05-26 07:59:53 +0000

    Changed Dependencied

--- unity_games/hooks.py
+++ unity_games/hooks.py
@@ -8,7 +8,7 @@ app_license = "mit"
 # Apps
 # ------------------
 
-required_apps = ["erpnext", "education", "payments", "edu_quality"]
+# required_apps = ["erpnext", "education", "payments", "edu_quality"]
 
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
